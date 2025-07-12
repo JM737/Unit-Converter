3d8a7ac24a1d279b2e2036c198a7e03424a296a3
JM737 <jm.billones028@gmail.com>
2025-07-13 00:05:49 +0800

    added a comment on UnitMain Class

--- UnitConverter/src/UnitMain.java
+++ UnitConverter/src/UnitMain.java
@@ -1,7 +1,7 @@
 /*TODO: 1. Create a class named UnitMain. // This class will contain the main method.
     2. Create a scanner to read user input. (Either it is a String or a int)
     3. Display a menu with a variety of unit conversion options. (String or a int)
-    - Polymorphism in one class or separate class (seperate class is better)
+    - Polymorphism in one class or separate class (separate class is better)
     - Length (meters, kilometers, miles) /done
     - Weight (grams, kilograms, pounds) /done
     - Temperature (Celsius, Fahrenheit) /ongoing
@@ -9,6 +9,7 @@
        4. Prompt the converted input */
 import java.util.Scanner;
 public class UnitMain{
+    //NEED TO LEARN ON HOW TO ACCESS THE WHOLE CLASSES WITH A SINGLE OBJECT//
     static Miles length = new Miles();
     static Scanner sc = new Scanner (System.in);
     public static void main(String[] args) {
